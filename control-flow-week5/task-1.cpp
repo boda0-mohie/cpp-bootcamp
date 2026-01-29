f410a7cb95e66ee7ca1af0a1b9d50eea7ba12fdd
boda0-mohie <bodemohie@gmail.com>
2026-01-29 22:03:50 +0200

    Edit TASK 1

--- control-flow-week5/task-1.cpp
+++ control-flow-week5/task-1.cpp
@@ -9,15 +9,9 @@ int main()
   float rate = 8.5f;
 
   // Your Condition Here
-  if (age > 18)
+  if (age > 18 && points > 500 && rate > 5)
   {
-    if (points > 500)
-    {
-      if (rate > 5)
-      {
-        cout << "Yes Age > 18 & Points > 500 & Rate > 5";
-      }
-    }
+    cout << "Yes Age > 18 & Points > 500 & Rate > 5";
   }
 
   // Output Needed If All Conditions Is True "Yes Age > 18 & Points > 500 & Rate > 5";
