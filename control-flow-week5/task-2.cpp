7cc0e83a5e3868865487a735dcc95e8c8d4afe4a
boda0-mohie <bodemohie@gmail.com>
2026-01-29 21:54:56 +0200

    Add gitignore

---
+++ control-flow-week5/task-2.cpp
@@ -0,0 +1,30 @@
+// TASK 2
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  // Test Case 3
+  int age = 20;
+  int points = 650;
+
+  if (age > 18)
+  {
+    cout << "Yes Age Is Ok" << endl;
+  }
+  else
+  {
+    cout << "No Age Is Not Ok" << endl;
+  }
+
+  if (points > 500)
+  {
+    cout << "Yes Points Is Ok" << endl;
+  }
+  else
+  {
+    cout << "No Points Is  Not Ok" << endl;
+  }
+
+  return 0;
+}
