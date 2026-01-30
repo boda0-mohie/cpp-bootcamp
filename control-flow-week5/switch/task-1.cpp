a87657ef4fb2866453e488bef3041405551091b7
boda0-mohie <bodemohie@gmail.com>
2026-01-31 01:19:50 +0200

    Switch Tasks Is Here

---
+++ control-flow-week5/switch/task-1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "Enter The Year: ";
+    cin >> year;
+
+    switch (year)
+    {
+    case 1982:
+        cout << "My Birth Day";
+        break;
+    case 1989:
+        cout << "My First Work";
+        break;
+    case 1995:
+        cout << "Windows 95";
+        break;
+    case 2000:
+        cout << "Windows Millennium";
+        break;
+
+    case 2002:
+        cout << "Created My vBulletin Forum";
+        break;
+
+    default:
+        cout << "No Events in This Year";
+        break;
+    }
+    return 0;
+}
+
+/*
+  1982 => "My Birth Day"
+  1989 => "My First Work"
+  1995 => "Windows 95"
+  2000 => "Windows Millennium"
+  2002 => "Created My vBulletin Forum"
+  Any Other Year => "No Events in This Year"
+*/
