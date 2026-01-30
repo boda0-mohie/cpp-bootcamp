491a465f499efacc97436b8e1d52951c20c1e105
boda0-mohie <bodemohie@gmail.com>
2026-01-30 23:10:46 +0200

    Tasks From 4 --> 7

---
+++ control-flow-week5/task-6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age = 40;
+    int points = 100;
+
+    if (age > 18 && sizeof(age) == 4 && points > 50)
+    {
+        cout << "Age Data Is 4 Bytes\n";
+        cout << "Age Is Ok\n";
+        cout << "Points Is Ok\n";
+    }
+
+    /*
+    Output Needed
+    "Age Data Is 4 Bytes"
+    "Age Is Ok"
+    "Points Is Ok"
+    */
+}
